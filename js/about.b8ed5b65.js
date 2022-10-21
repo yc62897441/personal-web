8daf11c8fc71c0da178c25693a6cb46668b655d3
yc62897441 <yc62897441@gmail.com>
2022-10-21 10:51:19 +0800

    deploy

---
+++ js/about.b8ed5b65.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpersonal_web"]=self["webpackChunkpersonal_web"]||[]).push([[443],{5399:function(n,e,s){s.r(e),s.d(e,{default:function(){return i}});var t=function(){var n=this;n._self._c;return n._m(0)},u=[function(){var n=this,e=n._self._c;return e("div",{staticClass:"about"},[e("h1",[n._v("This is an about page")])])}],a=s(1001),l={},r=(0,a.Z)(l,t,u,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.b8ed5b65.js.map
